9a0d36b47112cf01a5eb857e06ec4f873b07d2bd
Derek <derekbelloni10@gmail.com>
2022-04-06 13:43:17 -0600

    project initialized

---
+++ Models/Account.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace gregs_list_two_sharp.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+    }
+}
